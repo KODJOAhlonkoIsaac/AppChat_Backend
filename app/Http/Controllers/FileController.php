a4a9bd55b36456104030dda649ce02828f9a2563
= <edemwomagno@gmail.com>
2024-10-07 01:19:30 +0000

    djjjjjjjjjjj

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -11,38 +11,48 @@ class FileController extends Controller
 {
     public function store(Request $request, $user_id, $group_id)
     {
-        $request->validate([
-            'file' => 'required|mimes:pdf,doc,docx,png,jpg,jpeg,txt|max:1048',
-        ]);
-
-        $file = $request->file('file');
-        $filename = $file->getClientOriginalName();
-        $mime_type = $file->getClientMimeType();
-        // $file->move(public_path('uploads'), $filename);
-        $path = $file->storeAs('uploads', $filename, 'public');
-        $size = $file->getSize();
-        // $file_size_kb = $file_size / 1024; // Conversion en kiloOctets
-        $file_size_mb = $size / (1024 * 1024); // Conversion en mégaOctets
-        $file_size = round($file_size_mb, 2);// Taille en KB, arrondie à 2 décimales; // Conversion en mégaoctets
-        $url = Storage::url($path);
-
-        $file = new File();
-
-        $file->user_id = $user_id;
-        $file->group_id = $group_id;
-
-        $file->file_path = $path;
-        $file->file_name = $filename;
-        $file->mime_type = $mime_type;
-        $file->file_size = $file_size;
-        // Mail::to($request->email)->send(new accountMail($request->name));
-        $file->save();
-
-        return response()->json([
-            'message' => 'File uploaded successfully', 
-            'filename' => $filename,
-            'url' => $url,
-            'file' => $file
-        ]);
+        try {
+            $request->validate([
+                'file' => 'required|mimes:pdf,doc,docx,png,jpg,jpeg,txt|max:1048',
+            ]);
+    
+            // Code de traitement du fichier
+            $file = $request->file('file');
+            $filename = $file->getClientOriginalName();
+            $mime_type = $file->getClientMimeType();
+            $path = $file->storeAs('uploads', $filename, 'public');
+            $size = $file->getSize();
+            $file_size_mb = $size / (1024 * 1024);
+            $file_size = round($file_size_mb, 2);
+            $url = Storage::url($path);
+    
+            $file = new File();
+            $file->user_id = $user_id;
+            $file->group_id = $group_id;
+            $file->file_path = $path;
+            $file->file_name = $filename;
+            $file->mime_type = $mime_type;
+            $file->file_size = $file_size;
+            $file->save();
+    
+            return response()->json([
+                'message' => 'File uploaded successfully',
+                'filename' => $filename,
+                'url' => $url,
+                'file' => $file
+            ]);
+            
+        } catch (\Illuminate\Validation\ValidationException $e) {
+            return response()->json([
+                'message' => 'Validation errors occurred.',
+                'errors' => $e->errors()
+            ], 422);  // Utiliser le code HTTP 422 pour les erreurs de validation
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'An error occurred.',
+                'error' => $e->getMessage()
+            ], 500);  // Utiliser le code HTTP 500 pour les erreurs internes du serveur
+        }
     }
+    
 }
--- app/Http/Controllers/groupeController.php
+++ app/Http/Controllers/groupeController.php
@@ -56,6 +56,5 @@ class groupeController extends Controller
             'message' => 'success',
             'groupe' => $groups->name,
         ], 201);
-
     }
 }
